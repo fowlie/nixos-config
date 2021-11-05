d375344f7cb1f20c4407209f90ea2a12b7bab716
Mats Faugli <mats.faugli@gmail.com>
2021-11-05 09:52:54 +0100

    Use nightly build of Neovim

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -8,8 +8,15 @@ let
   ];
 in
 {
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
   programs.neovim = {
     enable       = true;
+    package      = pkgs.neovim-nightly;
     plugins      = myVimPlugins;
     vimAlias     = true;
     vimdiffAlias = true;
