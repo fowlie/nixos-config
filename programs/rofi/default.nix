385e2792e8093c84b3828307d18fa78acadeb402
Mats Faugli <mats.faugli@gmail.com>
2021-11-07 18:12:45 +0100

    Remove deprecated option

--- programs/rofi/default.nix
+++ programs/rofi/default.nix
@@ -3,7 +3,6 @@
 {
   programs.rofi = {
     enable = true;
-    scrollbar = false;
     package = pkgs.rofi.override { plugins = [ pkgs.rofi-emoji ]; };
     terminal = "${pkgs.alacritty}/bin/alacritty";
   };
