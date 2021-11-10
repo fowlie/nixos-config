cf1b12b7353dcc88fecd90d090040bd6434007f3
Mats Faugli <mats.faugli@gmail.com>
2021-11-10 18:47:27 +0100

    Make IntelliJ splash window float

--- programs/xmonad/config.hs
+++ programs/xmonad/config.hs
@@ -2,6 +2,9 @@ import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Run (runInTerm)
 
+-- for making dialog windows float
+import XMonad.Hooks.ManageHelpers
+
 -- for window spacing
 import XMonad.Layout.Spacing
 
@@ -12,6 +15,7 @@ main = xmonad $ def
   -- Bool|smartBorder Border|screenBorder Bool|screenBorderEnabled Border|windowBorder
   , layoutHook = spacingRaw False (Border 0 10 10 10) True (Border 10 10 10 10) True $
                  layoutHook def
+  , manageHook = myManageHook
   }
   `additionalKeysP`
   [ ("M-p", spawn appLauncher)
@@ -22,3 +26,9 @@ main = xmonad $ def
 myTerminal = "alacritty" 
 myBorderWidth = 3
 appLauncher = "rofi -modi drun,ssh,window,emoji -show drun -show-icons -theme gruvbox-dark"
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+  [ className =? "jetbrains-idea-ce" <&&> name =? "win0" --> doFloat
+  , isDialog                                             --> doFloat
+  ] where name = stringProperty "WM_NAME"
