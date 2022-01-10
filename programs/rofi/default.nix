e3a61126641f9e0791fb9cff2aa8d5f7371ea6e4
Mats Faugli <mats.faugli@gmail.com>
2022-01-10 22:14:45 +0100

    Bigger font, gruvbox theme

--- programs/rofi/default.nix
+++ programs/rofi/default.nix
@@ -5,5 +5,7 @@
     enable = true;
     package = pkgs.rofi.override { plugins = [ pkgs.rofi-emoji ]; };
     terminal = "${pkgs.alacritty}/bin/alacritty";
+    font = "FiraCode Nerd Font 30";
+    theme = "gruvbox-theme-soft";
   };
 }
