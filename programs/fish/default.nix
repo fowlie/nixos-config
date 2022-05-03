dbb6575f8bc954101e67eecdd115184be066c0c6
Mats Faugli <mats.faugli@gmail.com>
2022-05-03 22:37:44 +0200

    Replace oh-my-fish with starship prompt

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -3,15 +3,6 @@
 {
   programs.fish = {
     enable = true;
-    plugins = [{
-      name = "foreign-env";
-      src = pkgs.fetchFromGitHub {
-        owner = "oh-my-fish";
-        repo = "plugin-foreign-env";
-        rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
-        sha256 = "1x1h65l8582p7h7w5986sc9vfd7b88a7hsi68dbikm090gz8nlxx";
-      };
-    }];
     shellAbbrs = {
       g      = "git";
       gs     = "git status";
